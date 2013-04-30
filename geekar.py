0c90124d8b18e821ff31d4ada4a4b8c8f4346d33
Aur Saraf <sonoflilit@gmail.com>
2013-05-01 00:56:17 +0300

    geekar canvas, just need photo

---
+++ geekar.py
@@ -0,0 +1,25 @@
+CANVAS = {
+    "customers": [x + " Fans" for x in ["Comics", "RPG", " MTG", "Anime"]] + [" Gamers"],
+    "values": [
+        "<i>Awesome</i> AR Geek Shop",
+        "Infinite shelf space for long-tail products",
+        " Products connected to info online",
+        "Products connected to geeks in other stores",
+        " Sense of community",
+        "Pleasure of random surprises",
+        " Easily to setup mobile shop",
+        "Platform for interesting promotions",
+    ],
+    "channels": ["Conventions as marketing", "Retail (our shops)", " Online", "Geek events we host"],
+    "relationships": ["Physical", "Online Community", " Promotions", "Geek Events"],
+    "revenues": ["Asset sales"],
+    "resources": ["Shops", "Registered users", " Merchandise", "Promotions"],
+    "activities": ["Sales", "Marketing", " Hosting events"],
+    "partners": [
+        "WotC?",
+        " Marvel?",
+        " DC?",
+        " TOKYOPOP ?"
+    ],
+    "costs": ["One time: Development", "Constant: Rent", "Initial Merchandise", "Employee salary"],
+}
